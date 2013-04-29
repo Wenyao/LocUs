778078b87708007e06fe7354b2340ba2a9f5b411
Teness <tenesszhuo@gmail.com>
2013-04-29 18:41:58 -0400

    make AdapterList.java compilable

--- Locus/src/com/example/locus/AdapterList.java
+++ Locus/src/com/example/locus/AdapterList.java
@@ -11,20 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import android.app.Activity;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import com.example.locus.entity.Sex;
 import com.example.locus.entity.User;
-<<<<<<< HEAD
-import com.sun.xml.internal.bind.CycleRecoverable.Context;
-import com.sun.xml.internal.ws.api.server.Adapter;
-=======
->>>>>>> 2953950c2d9b2af78ded876166d4dbbd03f2cddf
 
-public class AdapterList extends Adapter<User> {
+public class AdapterList extends ArrayAdapter<User> {
   
 	Context context; 
     int layoutResourceId;    
@@ -72,14 +62,6 @@ public class AdapterList extends Adapter<User> {
         
         return row;
     }
-
-	@Override
-	protected User createToolkit() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-    
-    
 }
   
   
