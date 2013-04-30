c8513c3c5809d28023785ed77e934b535ab0935d
Teness <tenesszhuo@gmail.com>
2013-04-30 16:55:59 -0400

    add voice recognition

--- Locus/src/com/example/locus/Chat.java
+++ Locus/src/com/example/locus/Chat.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,6 +36,8 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 	private List<Message> msg;
 	private ChatAdapter chatAdapter;
 	User oppUser;
+	protected static final int RESULT_SPEECH = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,8 +52,8 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 		chatView = (ListView) findViewById(R.id.listViewChat);
 		sendButton = (Button) findViewById(R.id.sendButton);
 		tv = (EditText) findViewById(R.id.chatText);
-		nameText = (TextView)findViewById(R.id.textView1);
-		msgText = (TextView)findViewById(R.id.chatText);
+		nameText = (TextView) findViewById(R.id.textView1);
+		msgText = (TextView) findViewById(R.id.chatText);
 
 		msg = new ArrayList<Message>();
 		chatAdapter = new ChatAdapter(this, R.layout.activity_chat_adapter, msg);
@@ -56,9 +62,9 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 	}
 
 	private void addItemsToList(Message m) {
-		//msg.add(m);
-		//chatAdapter.clear();
-		
+		// msg.add(m);
+		// chatAdapter.clear();
+
 		chatAdapter.add(m);
 		chatAdapter.notifyDataSetChanged();
 	}
@@ -70,13 +76,53 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 		return true;
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+
+		case R.id.voice:
+			Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+			intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+			try {
+				startActivityForResult(intent, RESULT_SPEECH);
+				tv.setText("");
+			} catch (ActivityNotFoundException a) {
+				Toast t = Toast.makeText(getApplicationContext(),
+						"Opps! Your device doesn't support Speech to Text",
+						Toast.LENGTH_SHORT);
+				t.show();
+			}
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case RESULT_SPEECH:
+			if (resultCode == RESULT_OK && null != data) {
+
+				ArrayList<String> text = data
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				tv.setText(text.get(0));
+			}
+			break;
+		}
+	}
+
 	@Override
 	public void onClick(View v) {
-		//Message m = new Message();
+		// Message m = new Message();
 		String messg = tv.getText().toString();
-		
+
 		SendMessageTask sendMessageTask = new SendMessageTask();
-		
+
 		User currentUser = CoreFacade.getInstance().getCurrentUser();
 		Message mesg = new Message(currentUser, oppUser, "Normal", messg);
 		addItemsToList(mesg);
@@ -84,21 +130,24 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 		tv.setText("");
 
 	}
-	private class SendMessageTask extends
-	AsyncTask<Message, Integer, Message> {
+
+	private class SendMessageTask extends AsyncTask<Message, Integer, Message> {
 		@Override
 		protected Message doInBackground(Message... params) {
-			CoreFacade.getInstance().sendMessage(params[0].getDst(), (String)params[0].getData());
+			CoreFacade.getInstance().sendMessage(params[0].getDst(),
+					(String) params[0].getData());
 			return params[0];
 		}
 
 		@Override
 		protected void onPostExecute(Message result) {
-			String str_text = String.format("msg sent.  msg = %s", result.toString());
+			String str_text = String.format("msg sent.  msg = %s",
+					result.toString());
 			Toast.makeText(getApplicationContext(), str_text, Toast.LENGTH_LONG)
-			.show();
+					.show();
 		}
 	}
+
 	@Override
 	public void onReceiveMessage(Message msg) {
 		// TODO refresh message list, check src
@@ -115,7 +164,7 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 	}
 
 	private class OnReceiveMessageUpdateUITask extends
-	AsyncTask<Message, Integer, Message> {
+			AsyncTask<Message, Integer, Message> {
 		@Override
 		protected Message doInBackground(Message... params) {
 			return params[0];
@@ -125,15 +174,15 @@ public class Chat extends Activity implements OnClickListener, IObserver {
 		protected void onPostExecute(Message result) {
 			String str_text = result.toString();
 			Toast.makeText(getApplicationContext(), str_text, Toast.LENGTH_LONG)
-			.show();
+					.show();
 			msg.removeAll(msg);
-			
-			List<Message> chats = CoreFacade.getInstance().getMessagesByUser(result.getSrc());
-			
-			for(Message s : chats){
+
+			List<Message> chats = CoreFacade.getInstance().getMessagesByUser(
+					result.getSrc());
+
+			for (Message s : chats) {
 				addItemsToList(s);
 			}
-
 
 		}
 	}
