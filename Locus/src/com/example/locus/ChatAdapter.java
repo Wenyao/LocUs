d99669bfbd7e90255e277068a9d10605f8210f41
sohamchokshi <shmchksh@gmail.com>
2013-05-01 22:25:01 -0400

    gui

--- Locus/src/com/example/locus/ChatAdapter.java
+++ Locus/src/com/example/locus/ChatAdapter.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.example.locus.core.CoreFacade;
 import com.example.locus.entity.Message;
+import com.example.locus.entity.User;
 
 public class ChatAdapter extends ArrayAdapter<Message> {
 
@@ -52,8 +56,24 @@ public class ChatAdapter extends ArrayAdapter<Message> {
         }
         
         Message msg = data.get(position);
-        holder.StringView.setText((String) msg.getData());
-        holder.NameView.setText((String) msg.getSrc().getName());
+        User user = CoreFacade.getInstance().getCurrentUser();
+        
+        if(msg.getSrc().getName().equals(user.getName())){
+        	
+        	holder.StringView.setGravity(Gravity.LEFT);
+        	holder.NameView.setGravity(Gravity.LEFT);
+        	holder.NameView.setTextColor(Color.GREEN);
+        	holder.StringView.setText((String) msg.getData());
+            holder.NameView.setText((String) msg.getSrc().getName());
+        	
+        }else{
+        	holder.StringView.setGravity(Gravity.RIGHT);
+        	holder.NameView.setGravity(Gravity.RIGHT);
+        	holder.NameView.setTextColor(Color.BLUE);
+        	holder.StringView.setText((String) msg.getData());
+            holder.NameView.setText((String) msg.getSrc().getName());
+        }
+       
 //        if(user.getSex() == Sex.Female)
 //        	holder.ImageView.setImageResource(R.drawable.femaleicon);
 //        else
